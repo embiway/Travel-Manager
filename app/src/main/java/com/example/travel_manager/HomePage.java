42d91e177640c74c40cbe4e64f1c8ff271890f9f
fast-progress <shubhamgupta002001@gmail.com>
2020-10-14 12:51:27 +0530

    Implemented Tourist place name fetching part

--- app/src/main/java/com/example/travel_manager/HomePage.java
+++ app/src/main/java/com/example/travel_manager/HomePage.java
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
 
+    EditText text ;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        text = (EditText)findViewById(R.id.textview);
+        button = (Button)findViewById(R.id.button);
     }
-
     public void openMapActivity(View view){
+        String city = String.valueOf(text.getText());
+        city = city.toLowerCase();
         Intent intent = new Intent(this , MainActivity.class);
+        intent.putExtra("CITY_NAME", city);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/travel_manager/TourismPlace.java
@@ -0,0 +1,15 @@
+package com.example.travel_manager;
+
+public class TourismPlace{
+    public String icon;
+    public String name;
+    public String vicinity;
+    public  String photoUrl;
+
+    public TourismPlace(String icon, String name, String vicinity, String photoUrl) {
+        this.icon = icon;
+        this.name = name;
+        this.vicinity = vicinity;
+        this.photoUrl = photoUrl;
+    }
+}
